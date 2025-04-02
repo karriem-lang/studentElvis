5456a3dd3feb0d276c5952e4cad305ac010ac685
karriem-lang <karriemlang@proton.me>
2025-04-02 03:24:30 -0400

    Initial Commit

---
+++ studentElvis/studentElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  studentElvisApp.swift
+//  studentElvis
+//
+//  Created by Karriem Lang on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct studentElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
